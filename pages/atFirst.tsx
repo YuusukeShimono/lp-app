9bc519c4d7453c04ea9eeb98abbcb920ed093f6f
jungle379 <jungle379@gmail.com>
2023-04-27 11:19:42 +0900

    ⚡️

--- pages/atFirst.tsx
+++ pages/atFirst.tsx
@@ -10,10 +10,12 @@ const AtFirst = () => {
       </Head>
       <Header />
       <main className="min-h-screen bg-green-300 pt-[150px] pb-[130px] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
-        <div className="text-2xl px-[10px]">はじめに</div>
+        <div className="text-2xl px-[10px]">はじめに(購入時の考え方)</div>
         <div className="font-bold px-5 py-5">
           <li>馬券は基本、単複勝負</li>
-          <li>損益を考えて、深みにハマっての自滅は防ぐ</li>
+          <li>上位人気から勝ち馬を見抜いて勝負する</li>
+          <li>穴馬が構想するローカルは軽視する</li>
+          <li>損益を考えて、深みにハマる自滅は防ぐ</li>
           <li>直近の結果より、血統・バイアスを重視</li>
           <li>重賞より、条件戦を狙う</li>
           <li>騎手・競馬場の特徴も認識の上、予想する</li>
--- pages/contact.tsx
+++ pages/contact.tsx
@@ -12,21 +12,24 @@ const Contact = () => {
       </Head>
       <Header />
       <main className="min-h-screen bg-green-300 pt-[150px] pb-[130px] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
-        <div className="text-2xl">メールでの問い合わせ</div>
-        <div className="text-red-700 my-10 underline text-xl font-bold hover:underline flex justify-center">
+        <div className="text-2xl mx-5">メールでの問い合わせ</div>
+        <div className="text-red-700 mt-10 mb-5 underline text-xl font-bold hover:underline flex justify-center">
           <a href="mailto:jungle379@gmail.com">メールはこちらをクリック</a>
         </div>
-        <div className="text-sm">
+        <div className="text-sm flex justify-center">
+          <Balancer>※メーラーが自動で起動します！</Balancer>
+        </div>
+        <div className="text-2xl mt-5 mx-5">予想の参照先(Twitter)</div>
+        <div className="my-5 ml-10 font-bold text-sm underline">
           <Balancer>
-            ※メーラーが起動しますので本文入力後、送信して下さい！
+            <Link href="https://twitter.com/GANMA71879454">
+              ①邪推師GANMA
+              （チャンネル登録者数10万人超の競馬YouTuber＆競馬ブロガー）
+            </Link>
           </Balancer>
         </div>
-        <div className="text-2xl mt-5">予想の参照先</div>
-        <div>
-          <Link href=""></Link>
-        </div>
-        <div>
-          <Link href=""></Link>
+        <div className="my-5 ml-10 font-bold text-sm underline">
+          <Link href="/https://twitter.com/offrecoroom">②覆面馬主7号</Link>
         </div>
       </main>
       <Footer />
