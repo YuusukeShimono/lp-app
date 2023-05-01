d21b4db06eeb629759e95f744e6309518b2c1483
jungle379 <jungle379@gmail.com>
2023-05-02 01:49:44 +0900

    ⚡️

--- pages/result/2304.tsx
+++ pages/result/2304.tsx
@@ -12,11 +12,11 @@ const Result04 = () => {
       <main className="min-h-screen bg-green-300 pt-[150px] pb-[130px] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
         <div className="text-2xl mx-5">23年4月の結果</div>
         <div className="mx-10 font-bold">単勝回収率</div>
-        <h1 className="mx-10">1000--670</h1>
-        <h2 className="mx-10">67%</h2>
+        <h1 className="mx-10">1000→670</h1>
+        <h2 className="mx-10">回収率:67%</h2>
         <div className="mt-10 mx-10 font-bold">複勝回収率</div>
-        <h1 className="mx-10">1000--1310</h1>
-        <h2 className="mx-10">131%</h2>
+        <h1 className="mx-10">1000→1310</h1>
+        <h2 className="mx-10">回収率:131%</h2>
       </main>
       <Footer />
     </>
--- pages/result/2305.tsx
+++ pages/result/2305.tsx
@@ -11,8 +11,12 @@ const Result05 = () => {
       <Header />
       <main className="min-h-screen bg-green-300 pt-[150px] pb-[130px] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
         <div className="text-2xl mx-5">23年5月の結果</div>
-        <div></div>
-        <div></div>
+        <div className="mx-10 font-bold">単勝回収率</div>
+        <h1 className="mx-10"></h1>
+        <h2 className="mx-10">回収率:%</h2>
+        <div className="mt-10 mx-10 font-bold">複勝回収率</div>
+        <h1 className="mx-10"></h1>
+        <h2 className="mx-10">回収率:%</h2>
       </main>
       <Footer />
     </>
