90e6de14e969adf62e13242c527162f9876c32d2
jungle379 <jungle379@gmail.com>
2023-05-22 02:58:31 +0900

    ⚡️

--- pages/horse/amante.tsx
+++ pages/horse/amante.tsx
@@ -8,13 +8,15 @@ const Amante = () => {
                 <title>アマンテビアンコ(2歳)</title>
             </Head>
             <main className="min-h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
-                <div className="text-2xl font-bold">アマンテビアンコ(2歳)</div>
+                <div className="text-2xl font-bold mt-5">
+                    アマンテビアンコ(2歳)
+                </div>
                 <div className="mt-10 mx-10 text-2xl">戦績</div>
                 <h1 className="mx-10 text-xl">
                     0-0-0-0(新馬) →6/17デビュー予定？
                 </h1>
                 <div className="mt-10 mx-10 text-2xl">直近の情報</div>
-                <h1 className="mx-10 text-sm my-5">
+                <h1 className="mx-10 text-xl my-5">
                     <Balancer>
                         5/21
                         <br />
