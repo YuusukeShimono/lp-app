fe8bd4b3837ab352a871cc8229327691a20be033
jungle379 <jungle379@gmail.com>
2023-05-24 11:13:25 +0900

    ⚡️

--- pages/horse/amante.tsx
+++ pages/horse/amante.tsx
@@ -5,24 +5,22 @@ const Amante = () => {
     return (
         <>
             <Head>
-                <title>アマンテビアンコ(2歳)</title>
+                <title>アマンテビアンコ</title>
             </Head>
             <main className="min-h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
                 <div className="text-2xl font-bold mt-5">
                     アマンテビアンコ(2歳)
                 </div>
                 <div className="mt-10 mx-10 text-2xl">戦績</div>
-                <h1 className="mx-10 text-xl">
-                    0-0-0-0(新馬) →6/17デビュー予定？
-                </h1>
+                <h1 className="mx-10 text-xl">0-0-0-0(新馬)</h1>
                 <div className="mt-10 mx-10 text-2xl">直近の情報</div>
                 <h1 className="mx-10 text-xl my-5">
                     <Balancer>
                         5/21
                         <br />
-                        助 手 美南坂稍 56.5 - 41.6 - 27.3 - 13.7 馬ナリ
+                        助 手 美南坂稍 56.6 - 41.6 - 27.3 - 13.7 馬ナリ
                         <br />
-                        アーレンダール(古馬２勝)馬ナリを0.4秒追走同入
+                        アーレンダール(古馬2勝)馬ナリを0.4秒追走同入
                     </Balancer>
                 </h1>
             </main>
