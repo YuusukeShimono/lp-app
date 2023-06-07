e402fcd3e962603b461c7997f5e6894ef7439763
jungle379 <jungle379@gmail.com>
2023-06-07 22:31:34 +0900

    🐛

--- pages/horse/amante.tsx
+++ pages/horse/amante.tsx
@@ -9,18 +9,17 @@ const Amante = () => {
             </Head>
             <main className="min-h-screen overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
                 <div className="text-2xl font-bold mt-5">
-                    アマンテビアンコ(2歳),6/17東京ダート1400m?
+                    アマンテビアンコ(2歳)
                 </div>
                 <div className="mt-10 mx-10 text-2xl">戦績</div>
                 <h1 className="mx-10 text-xl">0-0-0-0(新馬)</h1>
                 <div className="mt-10 mx-10 text-2xl">直近の情報</div>
                 <h1 className="mx-10 text-xl my-5">
                     <Balancer>
-                        5/28
+                        6/4
+                        <br />助 手
                         <br />
-                        助 手 美南坂良 58.9 - 42.4 - 27.8 - 13.5 馬ナリ
-                        <br />
-                        エリカエスティーム(新馬)馬ナリに1.3秒先行同入
+                        美南Ｗ重 73.8 - 57.4 - 42.0 - 13.2 位置[5] 馬ナリ
                     </Balancer>
                 </h1>
             </main>
