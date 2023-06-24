3778cb395a88dd07ac5f200cd417501eab2e7086
jungle379 <jungle379@gmail.com>
2023-06-24 16:03:38 +0900

    🐛

--- pages/result/2306.tsx
+++ pages/result/2306.tsx
@@ -12,10 +12,10 @@ const Result06 = () => {
             <main className="min-h-screen bg-green-300 pt-[150px] pb-[130px] overflow-y-scroll scrollbar-thin scrollbar-thumb-gray-500 scrollbar-track-gray-100">
                 <div className="text-2xl mx-5">23年6月の結果</div>
                 <div className="mx-10 font-bold">単勝回収率</div>
-                <h1 className="mx-10">2500-3740</h1>
+                <h1 className="mx-10">3000-3740</h1>
                 <h2 className="mx-10">回収率:150%</h2>
                 <div className="mt-10 mx-10 font-bold">複勝回収率</div>
-                <h1 className="mx-10">2500-2930</h1>
+                <h1 className="mx-10">3000-2930</h1>
                 <h2 className="mx-10">回収率:117%</h2>
             </main>
             <Footer />
